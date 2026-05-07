1140584baa7a4d2f7ce243432bedc8b95dff69d0
mohammad-oli <165128206+mohammad-oli56@users.noreply.github.com>
2026-05-07 14:03:47 +0600

    Update solutions.ts

--- solutions.ts
+++ solutions.ts
@@ -3,21 +3,12 @@ function filterEvenNumbers(numbers: number[]): number[] {
   return numbers.filter((num) => num % 2 === 0);
 }
 
-// Output
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
-
-//--------------------------------------------------------------
 
 // Problem 2
 function reverseString(str: string): string {
   return str.split("").reverse().join("");
 }
 
-// Output
-console.log(reverseString("typescript")); 
-
-
-//---------------------------------------------------------------
 
 // Problem 3
 type StringOrNumber = string | number;
@@ -30,29 +21,19 @@ function checkType(value: StringOrNumber): string {
   }
 }
 
-// Sample Output
-console.log(checkType("Hello"));
-
-// Sample Output
-console.log(checkType(42));
-
-//---------------------------------------------------------------
 
 // Problem 4
 function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
   return obj[key];
 }
 
-// Sample Output
+
 const user = {
   id: 1,
   name: "John Doe",
   age: 21,
 };
 
-console.log(getProperty(user, "name"));
-
-//---------------------------------------------------------------
 
 // Problem 5
 interface Book {
@@ -68,16 +49,13 @@ function toggleReadStatus(book: Book): Book & { isRead: boolean } {
   };
 }
 
-// Sample Output
+
 const myBook = {
   title: "TypeScript Guide",
   author: "Jane Doe",
   publishedYear: 2024,
 };
 
-console.log(toggleReadStatus(myBook));
-
-//---------------------------------------------------------------
 
 // Problem 6
 class Person {
@@ -103,17 +81,12 @@ class Student extends Person {
   }
 }
 
-// Sample Output
+
 const student = new Student("Alice", 20, "A");
 
-console.log(student.getDetails());
-
-//---------------------------------------------------------------
 
 // Problem 7
 function getIntersection(arr1: number[], arr2: number[]): number[] {
   return arr1.filter((num) => arr2.includes(num));
 }
 
-// Sample Output
-console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]));
